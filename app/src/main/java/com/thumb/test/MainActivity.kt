fc41e10b0e276d4c9475c28452a7196374ee74e2
DESKTOP-84FOQGR\BreezyWeezy <didoka096@gmail.com>
2025-07-09 02:50:48 +0100

    new changes

--- app/src/main/java/com/thumb/test/MainActivity.kt
+++ app/src/main/java/com/thumb/test/MainActivity.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.exoplayer2.*
@@ -82,7 +85,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         playerView = findViewById(R.id.playerView)
         fpsText = findViewById(R.id.fpsText)
